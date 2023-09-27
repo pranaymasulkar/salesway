d818ba3fc04dafba1d6624af907778c8591af233
pranay <vickymasulkar@gmail.com>
2023-09-27 19:49:23 +0530

    fetch data using post api

--- src/component/dashboard/DataTableTr.jsx
+++ src/component/dashboard/DataTableTr.jsx
@@ -3,70 +3,58 @@ import axios from "axios";
 const DataTableTr = () => {
   const [tbdata, setTbdata] = useState([]);
 
-  const getData = () => {
-    axios
-      .get(
-        `http://ec2-34-227-25-226.compute-1.amazonaws.com:8000/sample_assignment_
-        api_3/`
-      )
-      .then((res) => {
-        const row = res.data;
-        setTbdata(row);
-        console.log(row);
-      })
-      .catch((error) => console.log(error));
-  };
+  // const getData = () => {
+  //   axios
+  //     .get(
+  //       `http://ec2-34-227-25-226.compute-1.amazonaws.com:8000/sample_assignment_
+  //       api_3/`
+  //     )
+  //     .then((res) => {
+  //       const row = res.data;
+  //       setTbdata(row);
+  //       console.log(row);
+  //     })
+  //     .catch((error) => console.log(error));
+  // };
 
   useEffect(() => {
-    getData();
+    // getData();
+
+    const url = `http://ec2-34-227-25-226.compute-1.amazonaws.com:8000/sample_assignment_
+  api_3/`;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    })
+      .then((res) => {
+        const row = res.response.json();
+        setTbdata(row);
+        console.log(row);
+        // Process the response data
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle any errors
+      });
   }, []);
 
-  const trdata = [
-    {
-      product: "camera mi 360",
-      solid_amount: 345,
-      unit_price: "$678",
-      ravanu: "$3456",
-      rating: "4.5",
-    },
-    {
-      product: "camera mi 360",
-      solid_amount: 345,
-      unit_price: "$678",
-      ravanu: "$3456",
-      rating: "4.5",
-    },
-    {
-      product: "camera mi 360",
-      solid_amount: 345,
-      unit_price: "$678",
-      ravanu: "$3456",
-      rating: "4.5",
-    },
-    {
-      product: "camera mi 360",
-      solid_amount: 345,
-      unit_price: "$678",
-      ravanu: "$3456",
-      rating: "4.5",
-    },
-  ];
   return (
     <>
-      {tbdata &&
-        tbdata.map((item, index) => {
-          return (
-            <>
-              <tr key={index}>
-                <td>{item.name}</td>
-                <td>{item.solid_amount}</td>
-                <td>{item.unit_price}</td>
-                <td>{item.ravanu}</td>
-                <td>{item.rating}</td>
-              </tr>
-            </>
-          );
-        })}
+      {tbdata.map((item, index) => {
+        return (
+          <tr key={index}>
+            <td>{item.name}</td>
+            <td>{item.solid_amount}</td>
+            <td>{item.unit_price}</td>
+            <td>{item.ravanu}</td>
+            <td>{item.rating}</td>
+          </tr>
+        );
+      })}
     </>
   );
 };
